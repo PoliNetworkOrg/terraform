65a665592533f6137a79a0d8233ba57741ecf2ca
Elia Maggioni <elia.maggioni@gmail.com>
2022-11-22 19:47:23 +0100

    comments

--- main.tf
+++ main.tf
@@ -33,4 +33,14 @@ module "argo_cd" {
 
 }
 
-#export KUBE_CONFIG_PATH="~/.kube/config"
+# HOW TO CONFIGURE INFRA
+# - create GROUP:
+# az group create --name GROUP
+# - create cluster K8S:
+# az aks create -g GROUP -n CLUSTER_NAME --enable-managed-identity --node-count 1 --enable-addons monitoring --enable-msi-auth-for-monitoring  --generate-ssh-keys --node-vm-size standard_a2_v2
+# - Create KeyVault:
+# az keyvault create --name kv-NAME --resource-group GROUP --location "westeurope"
+# - Configure kubectl
+# az aks get-credentials --resource-group GROUP --name CLUSTER_NAME
+# - Configure KUBE_CONFIG_PATH for kubectl
+# export KUBE_CONFIG_PATH="~/.kube/config"
