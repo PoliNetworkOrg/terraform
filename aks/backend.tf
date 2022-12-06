1922b116eb151c849f874c44ea098f6a21f48189
Elia Maggioni <elia.maggioni@gmail.com>
2022-12-06 17:10:12 +0100

    remote state

--- aks/backend.tf
+++ aks/backend.tf
@@ -1,9 +1,22 @@
 terraform {
-  cloud {
+  backend "remote" {
+    hostname     = "app.terraform.io"
     organization = "polinetworkAPS"
 
     workspaces {
-      name = "polinetwork-dev"
+      name = "aks-polinetwork"
+    }
+  }
+}
+
+data "terraform_remote_state" "aks" {
+  backend = "remote"
+
+  config = {
+    hostname     = "app.terraform.io"
+    organization = "polinetworkAPS"
+    workspaces = {
+      name = "aks-polinetwork"
     }
   }
 }
