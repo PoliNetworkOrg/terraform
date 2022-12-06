c90997726b17a3c8f2704c6fd32abf64dd2ac2fa
Elia Maggioni <elia.maggioni@gmail.com>
2022-12-06 14:25:28 +0100

    provider

--- aks/providers.tf
+++ aks/providers.tf
@@ -37,3 +37,10 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].cluster_ca_certificate)
   }
 }
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.credentials.kube_config[0].host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.credentials.kube_config[0].cluster_ca_certificate)
+}
