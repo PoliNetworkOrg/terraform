03011d75b5a3f70ffc27fd15d94d98aeb4be58d7
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-02-10 20:22:29 +0100

    fix: update kv access policy with new CI service principals

--- keyvault/keyvault.tf
+++ keyvault/keyvault.tf
@@ -16,24 +16,25 @@ resource "azurerm_key_vault" "keyvalue" {
     ip_rules       = var.allowed_ips
   }
 
-  access_policy = [{
-    tenant_id = var.tenant_id
-    object_id = "6b6a6388-c024-450b-80b4-9dcfa474c9f0"
+  access_policy = [
+    {
+      tenant_id = var.tenant_id
+      object_id = "6b6a6388-c024-450b-80b4-9dcfa474c9f0"
 
-    key_permissions = ["Get", "List", "Update", "Create", "Import", "Delete",
-      "Recover", "Backup", "Restore"
-    ]
+      key_permissions = ["Get", "List", "Update", "Create", "Import", "Delete",
+        "Recover", "Backup", "Restore"
+      ]
 
-    secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup",
-      "Restore"
-    ]
+      secret_permissions = ["Get", "List", "Set", "Delete", "Recover", "Backup",
+        "Restore"
+      ]
 
-    certificate_permissions = ["Get", "List", "Update", "Create", "Import",
-      "Delete", "Recover", "Backup", "Restore", "ManageContacts", "ManageIssuers",
-      "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", "Purge"
-    ]
-    storage_permissions = [],
-    application_id      = ""
+      certificate_permissions = ["Get", "List", "Update", "Create", "Import",
+        "Delete", "Recover", "Backup", "Restore", "ManageContacts", "ManageIssuers",
+        "GetIssuers", "ListIssuers", "SetIssuers", "DeleteIssuers", "Purge"
+      ]
+      storage_permissions = [],
+      application_id      = null
     },
     {
       key_permissions = [
@@ -45,9 +46,40 @@ resource "azurerm_key_vault" "keyvalue" {
         "Get",
         "List",
       ]
-      application_id          = "",
+      application_id          = null,
       storage_permissions     = [],
       tenant_id               = var.tenant_id,
       certificate_permissions = []
-  }]
+    },
+    {
+      certificate_permissions = []
+      key_permissions = [
+        "Get",
+        "List",
+      ]
+      object_id = "81dd9fd1-ea71-420a-9f8a-8cbb74f479a6"
+      secret_permissions = [
+        "Get",
+        "List",
+      ]
+      application_id      = null,
+      storage_permissions = []
+      tenant_id           = var.tenant_id
+    },
+    {
+      certificate_permissions = []
+      key_permissions = [
+        "Get",
+        "List",
+      ]
+      object_id = "f220ce5b-e174-413d-b6f8-04e214b85d76"
+      secret_permissions = [
+        "Get",
+        "List",
+      ]
+      application_id      = null,
+      storage_permissions = []
+      tenant_id           = var.tenant_id
+    },
+  ]
 }
