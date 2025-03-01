649032e940c56e5a06904f00423bb3b65bd3ada5
Lorenzo <lorenzo.corallo@outlook.it>
2025-03-01 20:56:08 +0100

    argocd git generator test

--- argocd/apps.tf
+++ argocd/apps.tf
@@ -21,6 +21,67 @@ resource "helm_release" "argo_cd" {
       tenant       = var.tenant
     })
   ]
+
+  set {
+    name  = "applicationSet.enabled"
+    value = "true"
+  }
+}
+
+resource "kubernetes_manifest" "git_generator_applicationset" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "ApplicationSet"
+    metadata = {
+      name      = "git-generator-app-set"
+      namespace = var.namespace
+    }
+    spec = {
+      generators = [
+        {
+          git = {
+            repoURL  = "https://github.com/PoliNetworkOrg/polinetwork-cd.git"
+            revision = "generator"
+            directories = [
+              {
+                path = "*"
+              }
+            ]
+          }
+        }
+      ]
+      template = {
+        metadata = {
+          name = "{{path.basename}}"
+        }
+        spec = {
+          project = "default"
+          source = {
+            repoURL  = "https://github.com/PoliNetworkOrg/polinetwork-cd.git"
+            targetRevision = "generator"
+            path           = "{{path}}"
+          }
+          destination = {
+            server    = "https://kubernetes.default.svc"
+            namespace = "{{path.basename}}"
+          }
+          syncPolicy = {
+            automated = {
+              prune    = true
+              selfHeal = true
+            }
+            syncOptions = [
+              "CreateNamespace=true"
+            ]
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.argo_cd
+  ]
 }
 
 resource "helm_release" "argocd_apps" {
