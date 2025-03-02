099335cb6f0bcd5595ecd217ab197409e4178db5
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-03-02 12:49:18 +0100

    upgrade argocd

--- argocd/apps.tf
+++ argocd/apps.tf
@@ -8,11 +8,14 @@ resource "helm_release" "argo_cd" {
   name       = "argo"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "5.36.1"
+  version    = "7.8.7"
+  dependency_update = true
   namespace  = var.namespace
 
   cleanup_on_fail  = true
   create_namespace = true
+  force_update = true
+  upgrade_install = true
 
   values = [
     templatefile("${path.module}/values/argo_cd.tftpl", {
@@ -21,73 +24,82 @@ resource "helm_release" "argo_cd" {
       tenant       = var.tenant
     })
   ]
-
-  set {
-    name  = "applicationSet.enabled"
-    value = "true"
-  }
 }
 
-resource "kubernetes_manifest" "git_generator_applicationset" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "ApplicationSet"
-    metadata = {
-      name      = "git-generator-app-set"
-      namespace = var.namespace
-    }
-    spec = {
-      generators = [
-        {
-          git = {
-            repoURL  = "https://github.com/PoliNetworkOrg/polinetwork-cd.git"
-            revision = "generator"
-            directories = [
-              {
-                path = "*"
-              }
-            ]
-          }
-        }
-      ]
-      template = {
-        metadata = {
-          name = "{{path.basename}}"
-        }
-        spec = {
-          project = "default"
-          source = {
-            repoURL  = "https://github.com/PoliNetworkOrg/polinetwork-cd.git"
-            targetRevision = "generator"
-            path           = "{{path}}"
-          }
-          destination = {
-            server    = "https://kubernetes.default.svc"
-            namespace = "{{path.basename}}"
-          }
-          syncPolicy = {
-            automated = {
-              prune    = true
-              selfHeal = true
-            }
-            syncOptions = [
-              "CreateNamespace=true"
-            ]
-          }
-        }
-      }
-    }
-  }
-
-  depends_on = [
-    helm_release.argo_cd
-  ]
-}
+#resource "kubernetes_manifest" "git_generator_applicationset" {
+#  manifest = {
+#    apiVersion = "argoproj.io/v1alpha1"
+#    kind       = "ApplicationSet"
+#    metadata = {
+#      name      = "git-generator-app-set"
+#      namespace = var.namespace
+#    }
+#    spec = {
+#      generators = [
+#        {
+#          git = {
+#            repoURL  = "https://github.com/PoliNetworkOrg/polinetwork-cd.git"
+#            revision = "generator"
+#            files = [
+#              {
+#                path = "**/config.json"
+#              }
+#            ]
+#          }
+#        }
+#      ]
+#      ignoreApplicationDifferences = [
+#        {
+#          jsonPointers = [
+#            "/spec/source/kustomize/images"
+#          ]
+#        }
+#      ]
+#      template = {
+#        metadata = {
+#          name = "{{path.basename}}"
+#          annotations = {
+#            "argocd-image-updater.argoproj.io/write-back-method" = "argocd"
+#            "argocd-image-updater.argoproj.io/argocd.force-update" = "true"
+#            "argocd-image-updater.argoproj.io/image-list" = "{{image.image-list}}" 
+#            "argocd-image-updater.argoproj.io/update-strategy" = "{{image.update-strategy}}"
+#          }
+#        }
+#        spec = {
+#          project = "default"
+#          source = {
+#            repoURL  = "https://github.com/PoliNetworkOrg/polinetwork-cd.git"
+#            targetRevision = "generator"
+#            path           = "{{path.basename}}/app"
+#          }
+#          destination = {
+#            server    = "https://kubernetes.default.svc"
+#            namespace = "{{path.basename}}"
+#          }
+#          syncPolicy = {
+#            automated = {
+#              prune    = true
+#              selfHeal = true
+#            }
+#            syncOptions = [
+#              "CreateNamespace=true"
+#            ]
+#          }
+#        }
+#      }
+#    }
+#  }
+#
+#  depends_on = [
+#    helm_release.argo_cd
+#  ]
+#}
 
 resource "helm_release" "argocd_apps" {
   name       = "argocd-apps"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argocd-apps"
+  version    = "2.0.2"
   namespace  = var.namespace
 
   cleanup_on_fail  = true
