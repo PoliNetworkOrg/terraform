ea2cdf231b5d0763482bf266f883852515fc2634
Lorenzo Corallo <66379281+lorenzocorallo@users.noreply.github.com>
2025-04-02 17:22:11 +0200

    fix: bot-mat pvc down (#63)

--- modules/bots-migration/bots.tf
+++ modules/bots-migration/bots.tf
@@ -18,82 +18,82 @@ resource "azurerm_managed_disk" "storage" {
   disk_size_gb         = var.persistent_storage_size_gi
 }
 
-resource "kubernetes_persistent_volume" "storage" {
-  count = var.persistent_storage ? 1 : 0
-
-  metadata {
-    name = "${random_uuid.volume.result}-bot-pv"
-  }
-  spec {
-    capacity = {
-      storage = "${var.persistent_storage_size_gi}Gi"
-    }
-    storage_class_name = "managed-csi"
-    access_modes       = ["ReadWriteOnce"]
-    persistent_volume_source {
-      csi {
-        driver        = "disk.csi.azure.com"
-        volume_handle = azurerm_managed_disk.storage[0].id
-      }
-    }
-  }
-
-  depends_on = [
-    azurerm_managed_disk.storage
-  ]
-}
-
-resource "kubernetes_persistent_volume_claim" "storage_pvc" {
-  count = var.persistent_storage ? 1 : 0
-  metadata {
-    name      = var.pvc_name == "" ? "bot-pvc" : var.pvc_name
-    namespace = var.bot_namespace
-  }
-  spec {
-    access_modes       = ["ReadWriteOnce"]
-    storage_class_name = "managed-csi"
-    resources {
-      requests = {
-        storage = "${var.persistent_storage_size_gi}Gi"
-      }
-    }
-    volume_name = kubernetes_persistent_volume.storage[0].metadata[0].name
-  }
-}
-
-resource "kubernetes_secret" "git_config" {
-  count = var.git_config ? 1 : 0
-  metadata {
-    name      = "git-config"
-    namespace = var.bot_namespace
-  }
-
-  data = {
-    "git_info.json" = jsonencode({
-      user        = var.git_user,
-      email       = var.git_email,
-      data_repo   = var.git_data_repo,
-      remote_repo = var.git_remote_repo,
-      path        = var.git_path
-    })
-    "ssh-key" = var.git_password
-  }
-}
-
-resource "kubernetes_secret" "git_ssh_config" {
-  count = var.git_config ? 1 : 0
-  metadata {
-    name      = "ssh-config"
-    namespace = var.bot_namespace
-  }
-
-  data = {
-    "config" = <<EOF
-Host github
- HostName github.com
- User git
- IdentityFile /git/ssh-key
- IdentitiesOnly yes
-EOF
-  }
-}
+# resource "kubernetes_persistent_volume" "storage" {
+#   count = var.persistent_storage ? 1 : 0
+# 
+#   metadata {
+#     name = "${random_uuid.volume.result}-bot-pv"
+#   }
+#   spec {
+#     capacity = {
+#       storage = "${var.persistent_storage_size_gi}Gi"
+#     }
+#     storage_class_name = "managed-csi"
+#     access_modes       = ["ReadWriteOnce"]
+#     persistent_volume_source {
+#       csi {
+#         driver        = "disk.csi.azure.com"
+#         volume_handle = azurerm_managed_disk.storage[0].id
+#       }
+#     }
+#   }
+# 
+#   depends_on = [
+#     azurerm_managed_disk.storage
+#   ]
+# }
+# 
+# resource "kubernetes_persistent_volume_claim" "storage_pvc" {
+#   count = var.persistent_storage ? 1 : 0
+#   metadata {
+#     name      = var.pvc_name == "" ? "bot-pvc" : var.pvc_name
+#     namespace = var.bot_namespace
+#   }
+#   spec {
+#     access_modes       = ["ReadWriteOnce"]
+#     storage_class_name = "managed-csi"
+#     resources {
+#       requests = {
+#         storage = "${var.persistent_storage_size_gi}Gi"
+#       }
+#     }
+#     volume_name = kubernetes_persistent_volume.storage[0].metadata[0].name
+#   }
+# }
+# 
+# resource "kubernetes_secret" "git_config" {
+#   count = var.git_config ? 1 : 0
+#   metadata {
+#     name      = "git-config"
+#     namespace = var.bot_namespace
+#   }
+# 
+#   data = {
+#     "git_info.json" = jsonencode({
+#       user        = var.git_user,
+#       email       = var.git_email,
+#       data_repo   = var.git_data_repo,
+#       remote_repo = var.git_remote_repo,
+#       path        = var.git_path
+#     })
+#     "ssh-key" = var.git_password
+#   }
+# }
+# 
+# resource "kubernetes_secret" "git_ssh_config" {
+#   count = var.git_config ? 1 : 0
+#   metadata {
+#     name      = "ssh-config"
+#     namespace = var.bot_namespace
+#   }
+# 
+#   data = {
+#     "config" = <<EOF
+# Host github
+#  HostName github.com
+#  User git
+#  IdentityFile /git/ssh-key
+#  IdentitiesOnly yes
+# EOF
+#   }
+# }
